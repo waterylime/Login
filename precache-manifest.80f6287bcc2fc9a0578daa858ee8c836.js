de3b7c13cbfeb02118ea7d6239dc3b5c5ee1fd4f
tzvik15 <tzvik15@yahoo.com>
2020-03-08 09:35:57 -0600

    Updates

---
+++ precache-manifest.80f6287bcc2fc9a0578daa858ee8c836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef09a34614f4fda8cb53b097f2621e8",
+    "url": "/Login/index.html"
+  },
+  {
+    "revision": "183a38964924553591fc",
+    "url": "/Login/static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "4f02a75c1add0af397d0",
+    "url": "/Login/static/css/main.09924d87.chunk.css"
+  },
+  {
+    "revision": "183a38964924553591fc",
+    "url": "/Login/static/js/2.5389122d.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/Login/static/js/2.5389122d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f02a75c1add0af397d0",
+    "url": "/Login/static/js/main.3f41c012.chunk.js"
+  },
+  {
+    "revision": "6ae4d093a608f9804b49",
+    "url": "/Login/static/js/runtime-main.05320bdd.js"
+  }
+]);
